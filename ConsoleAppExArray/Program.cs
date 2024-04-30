988e3b682f75504167b2a382b42a63a1c96a8649
Usuario <Usuario@CPD02-M01>
2024-04-30 10:41:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppExArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = { "João", "Pedro", "Maria" };
+
+            Console.WriteLine("Digite um número de 0 a 2: ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O nome selecionado foi: " + nome[i]);
+            Console.ReadKey();
+        }
+    }
+}
